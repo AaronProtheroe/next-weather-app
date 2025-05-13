2af08391e4bb71dfb3f432e2d620c680fd615ae0
Aaron Protheroe <Aaron.protheroe@sovtech.com>
2025-05-13 11:21:39 +0200

    minor fixes

--- src/app/api/weather/route.ts
+++ src/app/api/weather/route.ts
@@ -18,7 +18,6 @@ export async function GET(request: Request) {
 
     return NextResponse.json(data);
   } catch (error) {
-    console.error("API call failed:", error);
     return NextResponse.json(
       { error: "Failed to fetch data" },
       { status: 500 }
