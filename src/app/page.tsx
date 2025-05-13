1656f76c4fe1679e269b776423c18b6142142525
Aaron Protheroe <Aaron.protheroe@sovtech.com>
2025-05-13 09:48:43 +0200

    added loading and empty states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import CustomInput from "@/components/Input";
+import WeatherCard from "@/components/WeatherCard";
 import { useQuery } from "@tanstack/react-query";
+import { useMemo, useState } from "react";
 
 async function getWeather(city: string) {
   const res = await fetch(`api/weather?city=${city}`);
@@ -9,15 +11,54 @@ async function getWeather(city: string) {
 }
 
 export default function Home() {
-  const { data, isLoading, error } = useQuery({
+  const { data, isFetching, error, refetch } = useQuery({
     queryKey: ["weather"],
-    queryFn: () => getWeather("London"),
+    queryFn: () => getWeather(city),
   });
+
+  const [city, setCity] = useState("");
+
+  const weatherData = useMemo(() => {
+    if (data && data.weather) {
+      return {
+        temperature: data.main.temp,
+        weather: data.weather[0].main,
+        description: data.weather[0].description,
+        city: data.name,
+      };
+    }
+    return null;
+  }, [data]);
+
+  console.log(data);
 
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center bg-gray-800">
       <h1 className="text-4xl text-gray-300 mb-8">Warp Weather App</h1>
-      <CustomInput placeholder="Enter a city name" />
+      <div className="flex justify-center items-center gap-4 mb-8">
+        <CustomInput
+          placeholder="Enter a city name"
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button
+          className="px-4 py-2 bg-gray-700 text-white rounded cursor-pointer hover:bg-gray-600"
+          onClick={() => refetch()}
+        >
+          Search
+        </button>
+      </div>
+      {weatherData === null && !isFetching && (
+        <h1 className="text-gray-300">No data found</h1>
+      )}
+      {isFetching && <h1 className="text-gray-300">Loading...</h1>}
+      {!isFetching && weatherData !== null && (
+        <WeatherCard
+          temperature={weatherData.temperature}
+          weather={weatherData.weather}
+          description={weatherData.description}
+          city={weatherData.city}
+        />
+      )}
     </div>
   );
 }
--- src/components/WeatherCard/index.tsx
+++ src/components/WeatherCard/index.tsx
@@ -1,7 +1,24 @@
 type WeatherCardProps = {
-  city: string;
+  city: string | null;
+  temperature: number | null;
+  weather: string | null;
+  description: string | null;
 };
 
-const WeatherCard = () => {
-  return <div></div>;
+const WeatherCard = ({
+  city,
+  temperature,
+  weather,
+  description,
+}: WeatherCardProps) => {
+  return (
+    <div className="bg-gray-500 p-10">
+      <h2 className="text-2xl text-gray-300 mb-4">Weather in {city}</h2>
+      <p className="text-xl text-gray-200">Temperature: {temperature}°C</p>
+      <p className="text-xl text-gray-200">Weather: {weather}</p>
+      <p className="text-xl text-gray-200">Description: {description}</p>
+    </div>
+  );
 };
+
+export default WeatherCard;
