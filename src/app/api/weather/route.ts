3ac1c33d238af8690a346347d90cacf5e3bf67e5
Aaron Protheroe <Aaron.protheroe@sovtech.com>
2025-05-13 11:52:21 +0200

    fixed error handling

--- src/app/api/weather/route.ts
+++ src/app/api/weather/route.ts
@@ -18,9 +18,8 @@ export async function GET(request: Request) {
 
     return NextResponse.json(data);
   } catch (error) {
-    return NextResponse.json(
-      { error: "Failed to fetch data" },
-      { status: 500 }
-    );
+    if (error) {
+      return error;
+    }
   }
 }
